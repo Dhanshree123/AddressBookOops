5a1bf08f4f884b2626a9ddf72844555dd2ba1cb6
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-09-25 00:32:59 +0530

    Add UC3

--- AddressBook.java
+++ AddressBook.java
@@ -11,8 +11,11 @@ public class AddressBook {
 		  list = new ArrayList<AddressBookContacts>();
 		  
 	  }
+	  
+		Scanner sc =new Scanner(System.in);
+		
 
-	  public AddressBookContacts create(String firstName,String lastName,String address,
+/*	  public AddressBookContacts create(String firstName,String lastName,String address,
 			  String city,String state,int zip,String ph_no,String email ) {
 		  
 		  AddressBookContacts contact = new AddressBookContacts(firstName,lastName,address,city,state,zip,ph_no,email );
@@ -23,7 +26,73 @@ public class AddressBook {
 	  public void addContactDetails(AddressBookContacts contact) {
 		  list.add(contact);
 	  }
-	  
+*/
+	 
+public void editContactDetails(String firstName) {
 
+	int pos=0;
+
+	for(int i=0;i<list.size();i++) {
+		if(list.get(i).getFirstName().equals(firstName))
+			pos = i;
+		
+	}
+	while(true)
+	  {	
+	      System.out.println("Choose the option to edit");
+	      System.out.println("1.Edit Last name");
+	      System.out.println("2.Edit Address");
+	      System.out.println("3.Edit City");
+	      System.out.println("4.Edit State");
+	      System.out.println("5.Edit Zip");
+	      System.out.println("6.Edit Phone Number");
+	      System.out.println("7.Edit Email");
+	      System.out.println("8.Exit");
+	      
+	      int choice=sc.nextInt();
+	
+	      if(choice==8)
+	    	break;
+	
+	      switch(choice) {
+	       case 1:
+	       System.out.println("New Last Name");
+		   list.get(pos).setLastName(sc.next());
+		   break;
+		   
+	       case 2:
+	       System.out.println("Enter Address for editing");
+	   	   list.get(pos).setaddress(sc.next());
+	   	   break;
+	   	   
+	       case 3:
+	       System.out.println("Enter city for editing");
+		   list.get(pos).setCity(sc.next());
+		   break;
+		   
+	       case 4:
+	       System.out.println("Enter state for editing");
+	 	   list.get(pos).setState(sc.next());
+		   break;
+		   
+	       case 5:
+	       System.out.println("Enter Zip for editing");
+		   list.get(pos).setZip(sc.nextInt());
+		   break;
+		   
+	       case 6:
+	       System.out.println("Enter Phone Number for editing");
+		   list.get(pos).setPh_no(sc.next());
+		   break;
+		   
+	       case 7:
+	       System.out.println("Enter email for editing");
+		   list.get(pos).setEmail(sc.next());
+		   break;
+	   	   
+	    }
+	  }
+    }
 }
+
 
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -11,29 +11,12 @@ public class AddressBookMain {
 		String firstName, lastName, address, city, state, ph_no, email;
 		int zip;
 		
-		System.out.println("Enter the first name");
-		 firstName = sc.next();
-		 System.out.println("Enter the last name");
-		 lastName = sc.next();
-		 System.out.println("Enter the Address");
-		 address = sc.next();
-		 System.out.println("Enter the city");
-		 city = sc.next();
-		 System.out.println("Enter the state");
-		 state = sc.next();
-		 System.out.println("Enter the zip");
-		 zip = sc.nextInt();
-		 System.out.println("Enter the Phone Number");
-		 ph_no = sc.next();
-		 System.out.println("Enter the Email Id");
-		 email = sc.next();
-		 
 		AddressBook a = new AddressBook();
-		AddressBookContacts c = a.create(firstName, lastName, address, city, state, zip, ph_no, email);
-		a.addContactDetails(c);
-		
-		System.out.println("Contact Added");
-
+		System.out.println("Enter the first name to edit");
+		 firstName = sc.next();
+		 a.editContactDetails(firstName);
+		 System.out.println("Contact Details Editted");
+		 
 	}
 
 }
