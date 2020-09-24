ca5b15ed8f7c8150919af2ff81c6f83a351cdf93
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-09-24 22:08:30 +0530

    Add Master

---
+++ AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.blz.AddressBook;
+
+import java.util.*;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+	}
+}
